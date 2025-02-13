4b484f69df75e00807684ff5081e3bdfd52a22cb
CalleAlexanderson <calle.alexanderson@telia.com>
2025-02-13 18:21:35 +0100

    Ändrade isbn till string så den kan valideras med min och max length

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 namespace Moment3.Models;
 
 public class Book{
@@ -7,7 +8,10 @@ public class Book{
     public string? Title {get; set;}
     [DisplayName("Serie")]
     public string? Series {get; set;}
-    public int ISBN {get; set;}
+
+    [MinLength(10, ErrorMessage = "ISBN måste vara mellan 10-13 tecken långt")]
+    [MaxLength(13)]
+    public string? ISBN {get; set;}
     public int? AuthorId {get; set;}
     [DisplayName("Författare")]
     public Author? Author {get; set;}
